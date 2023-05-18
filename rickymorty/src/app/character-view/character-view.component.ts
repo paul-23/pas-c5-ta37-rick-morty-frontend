b0e34724b374c26f07d59262ba979b23ad54405e
Paul S <62121921+paul-23@users.noreply.github.com>
2023-05-18 18:05:39 +0200

    TA37 - Commit final

---
+++ rickymorty/src/app/character-view/character-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-view',
+  templateUrl: './character-view.component.html',
+  styleUrls: ['./character-view.component.css']
+})
+export class CharacterViewComponent {
+
+}
